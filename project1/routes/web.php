c2de6cb483a23b0f58bef777cb319db9bfa1bb88
Sweety3001 <sweetylog19@gmail.com>
2026-03-18 11:43:27 +0530

    cookies

--- project1/routes/web.php
+++ project1/routes/web.php
@@ -14,7 +14,7 @@ Route::get("/contact",function(){
     return "<h1 style='color: blue;'>This is Contact</h1>";
 });
 
-Route::get("/data",function(){
+Route::get("/data1",function(){
     $name="Sweety";
     return $name;
 });
@@ -81,4 +81,55 @@ Route::get('/home', function () {
     return view('home');
 });
 
+Route::get('/data', function () {
+    return view('data', ['name' => 'Sweety', 'age' => 20]);
+});
+
+Route::get("/withdata",function(){
+    return view('data')->with('name','Sweety')->with('age',20);
+});
+
+Route::get("/compact",function(){
+    $name="Sweety";
+    $age=20;
+    return view('data',compact('name','age'));
+});
+
+Route::get("product",function(){
+    return "Hello this is product page";
+});
+Route::get("list",function(){
+    return redirect('product');
+});
+
+
+//redirect with parameter
+Route::get("product/{name}",function($name){
+    return "Hello this is product page Mr. $name";
+});
+Route::get("list/{name}",function($name){
+    return redirect("product/$name");
+});
+
+
+Route::get("product2/{name?}",function($name=null){
+    return "Hello this is product page Mr. $name";
+});
+Route::get("list2/{name?}",function($name=null){
+    return redirect("product2/$name");
+});
+
+
+Route::get("header",function(){
+    return response("Hello this is header")
+    ->header('Content-Type','text/plain')
+    ->header("Author","Sweety");
+});
+
+
+//cookies
+Route::get("setcookie",function(){
+    return response("cookie set successfully")
+    ->cookie("Author","Sweety",60);
+});
 ?>
